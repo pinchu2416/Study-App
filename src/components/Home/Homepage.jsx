77a9a4c51fa59f0ef8df648d02cb1760c8fafdf8
Princess Priyadarsini <princess.priyadarsini333@gmail.com>
2025-03-14 15:41:18 +0530

    Updated project structure, modified index.html & App.jsx, removed unnecessary files

---
+++ src/components/Home/Homepage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return (
+      <div>
+        <h2 class="text-6xl text-blue-950">This is the Home Page</h2>
+        <p >Welcome to my Solid.js app!</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
